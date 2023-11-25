001fefa9d54bc861cc310a1aefb7d763cf42f0d3
tener-ilistom <85983974+tener-ilistom@users.noreply.github.com>
2023-11-25 12:22:11 +0200

    Page setup, images and logos

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Logo from "../assets/Logo.svg";
+
+export default function Header() {
+  return <img src={Logo} alt="Little Lemon logo" />;
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav>
+      <li>HOME</li>
+      <li>ABOUT</li>
+      <li>MENU</li>
+      <li>RESERVATIONS</li>
+      <li>ORDER ONLINE</li>
+      <li>LOGIN</li>
+    </nav>
+  );
+}
